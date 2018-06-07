49d94e5a2beccc5c6266665e423963d1d6b896c3
ADinushika <amalidinushika93@gmail.com>
2018-06-07 12:45:02 +0530

    #16 Re Correct the about_us.php Page

--- about_us.php
+++ about_us.php
@@ -23,29 +23,6 @@ and open the template in the editor.
     <?php
     include 'header.php';
     ?>
-        <nav class="navbar navbar-default navbar-fixed-top">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#myNavbar">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <h3><a class="scrollto" href="#">ADcreations</a></h3>
-
-                </div>
-                <div id="myNavbar" class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li class="menu-active"><a href="index.php"><h5><strong>HOME</strong></h5></a></li>
-                        <li class=""><a href="#myPage"><h5><strong>ABOUT US</strong></h5></a></li>
-                        <li class=""><a href="#myPage"><h5><strong>SERVICES</strong></h5></a></li>
-                        <li class=""><a href="gallery.php"><h5><strong>GALLERY</strong></h5></a></li>
-                        <li class=""><a href="alert.php"><h5><strong>REGISTER</strong></h5></a></li>
-
-                    </ul>
-                </div>
-            </div>
-        </nav>
 <!--/Header-->
 
         <section class="breadcrumbs-custom bg-image context-dark section-overlay-1">
@@ -89,94 +66,6 @@ and open the template in the editor.
         <?php
         include 'footer.php';
         ?>
-       
-        <footer id="footer">
-            <div class="footer-top">
-                <div class="container text-justify">
-                    <div class="row">
-                        <div class="col-lg-3 col-md-6 footer-info">
-                            <h3>ADcreations</h3>  
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
-
-                            </p>
-                        </div>
-                        <div class="col-lg-3 col-md-6 footer-links">
-                            <h4>Usefull Links<hr></h4>
-                            <ul>
-                                <li><i class="fa fa-chevron-right"></i>
-                                    <a href="#">Home</a>
-                                </li>
-                                <li><i class="fa fa-chevron-right"></i>
-                                    <a href="#">About Us</a>
-                                </li>
-                                <li><i class="fa fa-chevron-right"></i>
-                                    <a href="#">Services</a>
-                                </li>
-                                <li><i class="fa fa-chevron-right"></i>
-                                    <a href="#">Team</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-3 col-md-6 footer-contact">
-                            <h4>Contact Us<hr></h4> 
-                            <p>
-                                No.205
-                                <br>
-                                Bokundara
-                                <br>
-                                Piliyandala
-                                <br>
-                                Sri Lanka
-                                <br>
-                                <strong>Phone:</strong>
-                                +94 1 234 514 785
-                                <br>
-                                <strong>Email:</strong>
-                                info@gmail.ocm
-                                <br>
-                            </p>
-                            <div class="social-links">
-                                <a class="twitter" href="#">
-                                    <i class="fa fa-twitter"></i>
-                                </a>
-                                <a class="facebook" href="#">
-                                    <i class="fa fa-facebook"></i>
-                                </a>
-                                <a class="instagram" href="#">
-                                    <i class="fa fa-instagram"></i>
-                                </a>
-                                <a class="google-plus" href="#">
-                                    <i class="fa fa-google-plus"></i>
-                                </a>
-
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-6 footer-newsletter">
-                            <h4>Our news letter<hr></h4>
-                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
-                            <form action="" method="post">
-                                <input name="email" type="email">
-                                <input value="subscribe" type="submit">
-                            </form>
-                        </div>
-                    </div>
-
-                </div>
-            </div> 
-            <div class="container">
-                <div class="copyright"><i class="glyphicon glyphicon-copyright-mark"></i>
-                    Copyright
-                    <strong>ADcreations</strong>
-                    .All Rights Reserved
-                </div>
-                <div class="credits">
-                    Designed By 
-                    <a href="https://bootstrapmade.com/">BootstrapMade</a>
-                </div>
-            </div>
-
-        </footer>
         <!--/Footer-->
         <a class="back-to-top" href="#" style="">
             <i class="fa fa-chevron-up" style="margin-top: 12px"></i></a>

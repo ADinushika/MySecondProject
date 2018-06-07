5a90f60f87a3c10d908c8a02c0dfb23d7b3b0b20
ADinushika <amalidinushika93@gmail.com>
2018-06-07 11:59:02 +0530

    #10 Create about_us.php page

---
+++ about_us.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Amali</title>
+
+        <link href="plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="plugins/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    </head>
+    <body>
+
+
+<!--Header-->
+    <?php
+    include 'header.php';
+    ?>
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <h3><a class="scrollto" href="#">ADcreations</a></h3>
+
+                </div>
+                <div id="myNavbar" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="menu-active"><a href="index.php"><h5><strong>HOME</strong></h5></a></li>
+                        <li class=""><a href="#myPage"><h5><strong>ABOUT US</strong></h5></a></li>
+                        <li class=""><a href="#myPage"><h5><strong>SERVICES</strong></h5></a></li>
+                        <li class=""><a href="gallery.php"><h5><strong>GALLERY</strong></h5></a></li>
+                        <li class=""><a href="alert.php"><h5><strong>REGISTER</strong></h5></a></li>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+<!--/Header-->
+
+        <section class="breadcrumbs-custom bg-image context-dark section-overlay-1">
+            <img src="images/banner2.jpg" alt=""/>
+            <div class="container text-center">
+                <div class="breadcrumbs-custom__main">
+                    <h1 class="breadcrumbs-custom__title">About Us</h1>
+                    <div class="breadcrumbs-custom__text">
+                        <h4>Who we are and what we do</h4>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <br>
+        <br>
+        <section class="section section section-md bg-white">
+            <div class="container text-justify">
+
+                <div class="row row-50 justify-content-center-flex-md-row-reverse align-items-center">
+                    <div class="col-lg-6">
+                        <p class="text-initial-letter">Welcome! Please keep in mind that this is a commercial website that lists wallets, exchanges and other bitcoin related info.</p>
+                        <div class="divider-modern">
+                        </div>
+                        <p>Fusce vestibulum, elit quis maximus consequat, nunc elit fermentum orci, ac porttitor nisl ligula vitae sem. In egestas consequat fermentum. Donec dignissim eros et tincidunt venenatis. Integer pulvinar dignissim purus nec ornare. Integer nec elit est. Curabitur vehicula, mauris vel aliquam luctus, nulla ex venenatis diam, non elementum lacus ante sit amet nunc. Nulla facilisi. Maecenas fringilla mauris est. Curabitur tempus dictum amet sed.
+                            Fusce vestibulum, elit quis maximus consequat, nunc elit fermentum orci, ac porttitor nisl ligula vitae sem. In egestas consequat fermentum. Donec dignissim eros et tincidunt venenatis. Integer pulvinar dignissim purus nec ornare. Integer nec elit est. Curabitur vehicula, mauris vel aliquam luctus, nulla ex venenatis diam, non elementum lacus ante sit amet nunc. Nulla facilisi. Maecenas fringilla mauris est. Curabitur tempus dictum amet sed.
+                        </p>
+
+                    </div>
+
+                    <br>
+                    <div class="col-lg-6">
+                        <center><img src="images/people.jpg" alt=""/></center>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+        <!-- Footer -->
+        <?php
+        include 'footer.php';
+        ?>
+       
+        <footer id="footer">
+            <div class="footer-top">
+                <div class="container text-justify">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6 footer-info">
+                            <h3>ADcreations</h3>  
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+
+                            </p>
+                        </div>
+                        <div class="col-lg-3 col-md-6 footer-links">
+                            <h4>Usefull Links<hr></h4>
+                            <ul>
+                                <li><i class="fa fa-chevron-right"></i>
+                                    <a href="#">Home</a>
+                                </li>
+                                <li><i class="fa fa-chevron-right"></i>
+                                    <a href="#">About Us</a>
+                                </li>
+                                <li><i class="fa fa-chevron-right"></i>
+                                    <a href="#">Services</a>
+                                </li>
+                                <li><i class="fa fa-chevron-right"></i>
+                                    <a href="#">Team</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-3 col-md-6 footer-contact">
+                            <h4>Contact Us<hr></h4> 
+                            <p>
+                                No.205
+                                <br>
+                                Bokundara
+                                <br>
+                                Piliyandala
+                                <br>
+                                Sri Lanka
+                                <br>
+                                <strong>Phone:</strong>
+                                +94 1 234 514 785
+                                <br>
+                                <strong>Email:</strong>
+                                info@gmail.ocm
+                                <br>
+                            </p>
+                            <div class="social-links">
+                                <a class="twitter" href="#">
+                                    <i class="fa fa-twitter"></i>
+                                </a>
+                                <a class="facebook" href="#">
+                                    <i class="fa fa-facebook"></i>
+                                </a>
+                                <a class="instagram" href="#">
+                                    <i class="fa fa-instagram"></i>
+                                </a>
+                                <a class="google-plus" href="#">
+                                    <i class="fa fa-google-plus"></i>
+                                </a>
+
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 footer-newsletter">
+                            <h4>Our news letter<hr></h4>
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
+                            <form action="" method="post">
+                                <input name="email" type="email">
+                                <input value="subscribe" type="submit">
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
+            </div> 
+            <div class="container">
+                <div class="copyright"><i class="glyphicon glyphicon-copyright-mark"></i>
+                    Copyright
+                    <strong>ADcreations</strong>
+                    .All Rights Reserved
+                </div>
+                <div class="credits">
+                    Designed By 
+                    <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                </div>
+            </div>
+
+        </footer>
+        <!--/Footer-->
+        <a class="back-to-top" href="#" style="">
+            <i class="fa fa-chevron-up" style="margin-top: 12px"></i></a>
+
+        <?php
+        // put your code here
+        ?>
+        <script src="js/jquery-3.3.1.js" type="text/javascript"></script>
+        <script src="plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+
+    </body>
+</html>

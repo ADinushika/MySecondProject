4583b2dc2e2a1c8942d4f98208926996415b13d0
ADinushika <amalidinushika93@gmail.com>
2018-06-07 12:39:13 +0530

    #15 Re Correct the index.php Page

--- index.php
+++ index.php
@@ -30,65 +30,6 @@ and open the template in the editor.
         <?php
         include 'slider.php';
         ?>
-        <div class="intro-container">
-            <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                <!-- Indicators -->
-
-
-                <!-- Wrapper for slides -->
-                <div class="carousel-inner">
-                    <div class="item active">
-                        <img src="images/image1.jpeg" alt=""/>
-                        <div class="carousel-caption">
-                            <h1>Welcome to AD Creations</h1>
-                            <p><h3>We are team of talanted designers making websites with Bootstrap</h3></p>
-                        </div>
-                    </div>
-
-                    <div class="item">
-                        <img src="images/image2.jpeg" alt=""/>
-                        <div class="carousel-caption">
-                            <h1>Welcome to AD Creations</h1>
-                            <p>We are team of talanted designers making websites with Bootstrap</p>
-                        </div>
-                    </div>
-
-                    <div class="item">
-                        <img src="images/image3.jpeg" alt=""/>
-                        <div class="carousel-caption">
-                            <h1>Welcome to AD Creations</h1>
-                            <p>We are team of talanted designers making websites with Bootstrap</p>
-                        </div>
-                    </div>
-
-                    <div class="item">
-                        <img src="images/image4.jpeg" alt=""/>
-                        <div class="carousel-caption">
-                            <h1>Welcome to AD Creations</h1>
-                            <p>We are team of talanted designers making websites with Bootstrap</p>
-                        </div>
-                    </div>
-
-                    <div class="item">
-                        <img src="images/image5.jpeg" alt=""/>
-                        <div class="carousel-caption">
-                            <h1>Welcome to AD Creations</h1>
-                            <p>We are team of talanted designers making websites with Bootstrap</p>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Left and right controls -->
-                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </div>
-        </div>
         <!-- /Slider -->
         
         <br>
